f2dc090cf6e61797b7b29183825c26310d194252
Vuconghiep <Vuconghip@gmail.com>
2025-03-02 14:22:26 +0700

    init project

---
+++ Bai 1.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+	float x;
+	printf("Nhap gia tri x: ");
+	
+	scanf("%f", &x);
+	
+	printf("x^2 = %.2f\n", pow(x,2));
+	printf("x^3 = %.2f\n", pow(x,3));
+	printf("x^4 = %.2f\n", pow(x,4));
+}
---
+++ Bai2.cpp
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+	int ngay,thang,nam;
+	printf("Nhap ngay thang nam: ");
+	scanf("%d %d %d", &ngay,&thang,&nam);
+	printf("Hom nay la ngay %d\\%d\\%d",ngay,thang,nam);
+}
