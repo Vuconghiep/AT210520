583553b745f5a49b19db9b30bc0c022fc2d401ca
Vũ Công Hiệp <vuconghip@gmail.com>
2025-03-24 10:09:47 +0700

    Add files via upload

---
+++ Bai2.2.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+	int a[10];
+	printf("Nhap 10 phan tu: ");
+	for(int i=0;i<10;i++)
+	scanf("%d",&a[i]);
+	printf("Mang vua nhap: ");
+	for(int i=9;i>=0;i--){
+		
+		printf("%d ",a[i]);
+	}
+}
